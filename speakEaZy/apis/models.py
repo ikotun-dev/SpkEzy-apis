f21871f08076d7e517308e2eb3bacf704528edf0
Ikotun Collins <danlogan2003@gmail.com>
2023-06-09 18:42:40 +0100

    migration of 'member' model

--- speakEaZy/apis/models.py
+++ speakEaZy/apis/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class Member(AbstractUser):
+    firstname = models.CharField(max_length=255, null=True, blank=True) # '''member first name'''
+    lastname = models.CharField(max_length=255, null=True, blank=True) # '''member last  name '''
+    username = models.CharField(max_length=255, null=True, blank=True, unique=True) # '''member username '''
+    user_id = models.IntegerField(default=0) # '''unique  user id '''
+    password = models.CharField(max_length=255, null=False, blank=False)  # '''user password '''
+    verified = models.BooleanField(default=False) 
+
+   # '''Setting UserName '''
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = [ ] 
+
+    def __str__(self):
+        return  f"self.username"
+
+class Conversation(models.Model):
+    pass
+
--- speakEaZy/speakEaZy/urls.py
+++ speakEaZy/speakEaZy/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api import urls 
+from apis import urls 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include('api.urls'))
+    path("api/", include('apis.urls'))
 ]
